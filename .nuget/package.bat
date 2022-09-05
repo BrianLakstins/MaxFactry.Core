1bdfba4a801e52c756d140e91c2c1eb70b7f4c9f
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2022-09-05 13:15:13 -0400

    Create NuGet package

---
+++ .nuget/package.bat
@@ -0,0 +1,6 @@
+rem Package the library for Nuget
+copy ..\MaxFactry.Core-NF-2.0\bin\Release\*.dll lib\net20\
+copy ..\MaxFactry.Core-NF-4.5.2\bin\Release\*.dll lib\net452\
+copy ..\MaxFactry.Core-NC-2.1\bin\Release\netcoreapp2.1\*.dll lib\netcoreapp2.1\
+
+c:\install\nuget\nuget.exe pack MaxFactry.Core.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
